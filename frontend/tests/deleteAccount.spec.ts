77f07fd2bb4e53e6d2309b7b29baa17cbe5aaef0
amirhosssein <real.amirhosssein0@gmail.com>
2025-11-20 14:34:34 +0330

    add delete account test

---
+++ frontend/tests/deleteAccount.spec.ts
@@ -0,0 +1,58 @@
+import { test, expect, Page } from '@playwright/test';
+
+
+const PASSWORD = 'Test1234#'
+
+async function getFakeEmail(page: Page): Promise<string> {
+  await page.goto('https://www.fakemailgenerator.com', { waitUntil: 'domcontentloaded' });
+  await page.locator('#domain-select').click();
+
+  const domain = (await page.locator('#domain-select').textContent())?.trim();
+  if (domain !== '@gustr.com') {
+    await page.locator('li >> text=@gustr.com').click();
+  }
+
+  await page.waitForTimeout(500);
+  await page.getByRole('button', { name: /^copy$/i }).click();
+  await page.waitForTimeout(500);
+  const email = await page.evaluate(() => navigator.clipboard.readText());
+  return email;
+}
+
+test.describe.serial('Sign Up and Delete Account @108', () => {
+
+  let email: string; 
+
+  test('Signup', async ({ page, context }) => {
+      const mailPage = page;
+      email = await getFakeEmail(mailPage); 
+
+      const task = await context.newPage();
+      await task.goto('http://localhost:3000');
+      await task.getByRole('link', { name: 'Sign Up' }).click();
+      await task.getByRole('textbox', { name: 'Username' }).fill(email);
+      await task.getByRole('textbox', { name: 'Email' }).fill(email);
+      await task.getByRole('textbox', { name: 'Password', exact: true }).fill(PASSWORD);
+      await task.getByRole('textbox', { name: 'Confirm Password' }).fill(PASSWORD);
+      await task.getByRole('button', { name: 'Sign Up' }).click();
+      await task.waitForTimeout(5000);
+      await task.getByRole('button', { name: 'Logout' }).click();
+  });
+
+test('Delete Account', async ({ page }) => {
+    await page.goto('http://localhost:3000'); 
+    await page.getByRole('link', { name: 'Login' }).click();
+    await page.getByRole('textbox', { name: 'Username' }).fill(email); 
+    await page.getByRole('textbox', { name: 'Password' }).fill(PASSWORD);
+    await page.getByRole('button', { name: 'Sign In' }).click();
+    await page.waitForTimeout(3000);
+    await page.getByRole('link', { name: 'Profile' }).click();
+    page.once('dialog', async dialog => {
+    console.log(`Dialog: ${dialog.message()}`);
+    await dialog.accept();
+    });
+    await page.getByRole('button', { name: 'Delete Account' }).click();
+    await page.waitForTimeout(2000);
+    await page.getByRole('navigation').getByRole('button', { name: 'Logout' }).click();
+    });
+})
